b1513036ba238db3d71c4bf85d79ce6818d16316
Ester <ester.tataje@gmail.com>
2020-06-05 16:02:43 -0700

    fixed container in styles.css

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("form#crush").submit(function(event) {
+    const age = parseInt($("input#age").val());
+    const gender = $("select#gender").val();
+
+  if (age >= 20 && gender === "male") {
+    $("#crush1").show();
+  } else if (age >=21 && gender === "female") {
+    $("#crush2").show();
+  } else if (age <=20 && gender === "male") {
+    $("#crush3").show();
+  } else {
+    $("#crush4").show();
+  }
+
+  event.preventDefault();
+  });
+});
